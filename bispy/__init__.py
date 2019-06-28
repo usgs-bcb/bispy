10bae060ce7a2b67a1c62835a89e280a8da7326a
sbristol@usgs.gov <sbristol@usgs.gov>
2019-06-28 12:09:15 -0600

    New SGCN module and AWS adds

--- bispy/__init__.py
+++ bispy/__init__.py
@@ -13,6 +13,7 @@ from . import tess
 from . import xdd
 from . import gap
 from . import iucn
+from . import sgcn
 
 # provide version, PEP - three components ("major.minor.micro")
 __version__ = pkg_resources.require("bispy")[0].version
---
+++ bispy/sgcn.py
@@ -0,0 +1,23 @@
+import requests
+from datetime import datetime
+
+
+class Search:
+    def __init__(self):
+        self.sgcn_spp_search_api = "https://sciencebase.usgs.gov/staging/bis/api/v1/swap/speciessearch"
+
+    def search(self, name):
+        record = {
+            "search_api": f"{self.sgcn_spp_search_api}?text={name}",
+            "search_date": datetime.utcnow().isoformat(),
+            "results": list()
+        }
+
+        r_search = requests.get(record["search_api"]).json()
+
+        record["search_results"] = len(r_search["hits"]["hits"])
+
+        for hit in r_search["hits"]["hits"]:
+            record["results"].append(hit["_source"]["properties"])
+
+        return record
